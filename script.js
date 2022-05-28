5912086505ed67a237df756d84466aa10e53ce1b
Juan Negrete <juan.negrete.dev@gmail.com>
2022-05-29 00:51:18 +0100

    Complete playRound function, make return value an object

--- script.js
+++ script.js
@@ -7,16 +7,35 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === 'rock') {
     switch (computerSelection) {
       case 'rock':
-        console.log(`It's a tie! Both players picked ${playerSelection}.`);
-        break;
+        return {message: `It's a tie! Both players picked ${playerSelection}.`, playerWon: false, computerWon: false};
       case 'paper':
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-        break;
+        return {message: `You lose! ${computerSelection} beats ${playerSelection}.`, playerWon: false, computerWon: true};
       case 'scissors':
-        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-        break;
+        return{message: `You win! ${playerSelection} beats ${computerSelection}.`, playerWon: true, computerWon: false};
+    }
+  } else if (playerSelection === 'paper') {
+    switch (computerSelection) {
+      case 'rock':
+        return{message: `You win! ${playerSelection} beats ${computerSelection}.`, playerWon: true, computerWon: false};
+      case 'paper':
+        return {message: `It's a tie! Both players picked ${playerSelection}.`, playerWon: false, computerWon: false};
+      case 'scissors':
+        return {message: `You lose! ${computerSelection} beats ${playerSelection}.`, playerWon: false, computerWon: true};
+    }
+  } else {
+    switch (computerSelection) {
+      case 'rock':
+        return {message: `You lose! ${computerSelection} beats ${playerSelection}.`, playerWon: false, computerWon: true};
+      case 'paper':
+        return{message: `You win! ${playerSelection} beats ${computerSelection}.`, playerWon: true, computerWon: false};
+      case 'scissors':
+        return {message: `It's a tie! Both players picked ${playerSelection}.`, playerWon: false, computerWon: false};
     }
   }
+}
+
+function playGame() {
+
 }
 
 playRound('rock', computerPlay());
