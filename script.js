ee19c7d36357a426da9ded5c2440f563b986ae43
Juan Negrete <juan.negrete.dev@gmail.com>
2022-05-29 01:34:29 +0100

    Add playGame function, works well

--- script.js
+++ script.js
@@ -1,43 +1,102 @@
 function computerPlay() {
-  const hands =['rock', 'paper', 'scissors'];
+  const hands = ["rock", "paper", "scissors"];
   return hands[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock') {
+  if (playerSelection === "rock") {
     switch (computerSelection) {
-      case 'rock':
-        return {message: `It's a tie! Both players picked ${playerSelection}.`, playerWon: false, computerWon: false};
-      case 'paper':
-        return {message: `You lose! ${computerSelection} beats ${playerSelection}.`, playerWon: false, computerWon: true};
-      case 'scissors':
-        return{message: `You win! ${playerSelection} beats ${computerSelection}.`, playerWon: true, computerWon: false};
+      case "rock":
+        return {
+          message: `It's a tie! Both players picked ${playerSelection}.`,
+          playerWon: false,
+          computerWon: false,
+        };
+      case "paper":
+        return {
+          message: `You lose! ${computerSelection} beats ${playerSelection}.`,
+          playerWon: false,
+          computerWon: true,
+        };
+      case "scissors":
+        return {
+          message: `You win! ${playerSelection} beats ${computerSelection}.`,
+          playerWon: true,
+          computerWon: false,
+        };
     }
-  } else if (playerSelection === 'paper') {
+  } else if (playerSelection === "paper") {
     switch (computerSelection) {
-      case 'rock':
-        return{message: `You win! ${playerSelection} beats ${computerSelection}.`, playerWon: true, computerWon: false};
-      case 'paper':
-        return {message: `It's a tie! Both players picked ${playerSelection}.`, playerWon: false, computerWon: false};
-      case 'scissors':
-        return {message: `You lose! ${computerSelection} beats ${playerSelection}.`, playerWon: false, computerWon: true};
+      case "rock":
+        return {
+          message: `You win! ${playerSelection} beats ${computerSelection}.`,
+          playerWon: true,
+          computerWon: false,
+        };
+      case "paper":
+        return {
+          message: `It's a tie! Both players picked ${playerSelection}.`,
+          playerWon: false,
+          computerWon: false,
+        };
+      case "scissors":
+        return {
+          message: `You lose! ${computerSelection} beats ${playerSelection}.`,
+          playerWon: false,
+          computerWon: true,
+        };
     }
   } else {
     switch (computerSelection) {
-      case 'rock':
-        return {message: `You lose! ${computerSelection} beats ${playerSelection}.`, playerWon: false, computerWon: true};
-      case 'paper':
-        return{message: `You win! ${playerSelection} beats ${computerSelection}.`, playerWon: true, computerWon: false};
-      case 'scissors':
-        return {message: `It's a tie! Both players picked ${playerSelection}.`, playerWon: false, computerWon: false};
+      case "rock":
+        return {
+          message: `You lose! ${computerSelection} beats ${playerSelection}.`,
+          playerWon: false,
+          computerWon: true,
+        };
+      case "paper":
+        return {
+          message: `You win! ${playerSelection} beats ${computerSelection}.`,
+          playerWon: true,
+          computerWon: false,
+        };
+      case "scissors":
+        return {
+          message: `It's a tie! Both players picked ${playerSelection}.`,
+          playerWon: false,
+          computerWon: false,
+        };
     }
   }
 }
 
 function playGame() {
-
+  let playerScore = 0;
+  let computerScore = 0;
+  let result;
+  while (true) {
+    if (playerScore == 5 || computerScore == 5) {
+      console.log("GAME FINISHED");
+      return;
+    } else {
+      result = playRound(computerPlay(), computerPlay());
+      if (result.playerWon || result.computerWon) {
+        if (result.playerWon) {
+          console.log(result.message);
+          playerScore++;
+          console.log(`PLAYER: ${playerScore} | COMPUTER: ${computerScore}`);
+        } else {
+          console.log(result.message);
+          computerScore++;
+          console.log(`PLAYER: ${playerScore} | COMPUTER: ${computerScore}`);
+        }
+      } else
+        console.log(
+          result.message +
+            "\n" +
+            `PLAYER: ${playerScore} | COMPUTER: ${computerScore}`
+        );
+    }
+  }
 }
-
-playRound('rock', computerPlay());
-playRound('rock', computerPlay());
-playRound('rock', computerPlay());
+playGame();
